9d87f296fd749a9b5467cf7021ae02e187c6df01
Lucas Mariano <lucas.carmo@localiza.com>
2021-05-01 14:02:26 -0300

    User Entity

---
+++ CleanArc.Domain/Entities/User.cs
@@ -0,0 +1,39 @@
+﻿using CleanArc.Domain.Shared.Entity;
+using Flunt.Validations;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using static CleanArc.Domain.Entities.User;
+
+namespace CleanArc.Domain.Entities
+{
+    public sealed class User : BaseEntity
+    {
+        public User(string name, string password, string login)
+        {
+            Name = name;
+            Password = password;
+            Login = login;
+            AddNotifications(new UserValidation(this));
+        }
+
+        public string Name { get; private set; }
+        public string Password { get; private set; }
+        public string Login { get; private set; }
+    }
+    internal class UserValidation : Contract<User>
+    {
+        public UserValidation(User user)
+        {
+            Requires()
+                .IsNotNullOrWhiteSpace(user.Name, "Name", "Name is Required")
+                .IsNotNullOrWhiteSpace(user.Login, "", "Login Invalid")
+                .IsNotNullOrWhiteSpace(user.Password, "", "Profession is Required");
+        }
+    }
+  
+}
---
+++ CleanArc.Domain/Shared/Exceptions/DomainException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArc.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public override string Message { get; }
+        public bool Error { get; }
+        public DomainException(string message) : base(message)
+        {
+            this.Message = message;
+        }
+        public DomainException(string message, bool error) : base(message)
+        {
+            this.Message = message;
+            this.Error = error;
+        }
+    }
+}
